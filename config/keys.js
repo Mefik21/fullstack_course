41ce7556dfd225edfb84d87bd5b66653f2d3d98b
Mefik21 <seregakarpenko@gmail.com>
2022-01-14 22:04:19 +0300

    1123

---
+++ config/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+    googleClientID:
+        '679577345197-vt3n44vgpa0n8tt5jlar593covvcbdjv.apps.googleusercontent.com',
+    project_id: 'emaily-338217',
+    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+    token_uri: 'https://oauth2.googleapis.com/token',
+    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+    googleClientSecret: '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',
+    redirect_uris: ['http://localhost:5000/auth/google/callback'],
+    javascript_origins: ['http://localhost:5000'],
+};
