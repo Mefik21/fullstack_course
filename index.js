9bedd3ef1021de1a80815ff187422b643fabe40d
Mefik21 <seregakarpenko@gmail.com>
2022-01-15 23:19:11 +0300

    1123123

--- index.js
+++ index.js
@@ -5,17 +5,31 @@ const keys = require('./config/keys');
 //test
 
 const app = express();
+
+
 passport.use(
     new GoogleStrategy(
         {
             clientID: keys.googleClientID,
             clientSecret: keys.googleClientSecret,
             callbackURL: '/auth/google/callback'
-        }, (accessToken) => {
-            console.log(accessToken);
+        },
+        (accessToken, refreshToken, profile, done) => {
+            console.log('accessToken', accessToken);
+            console.log('refreshToken', refreshToken);
+            console.log('profile', profile);
         }
     )
 );
+
+app.get(
+    '/auth/google/',
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 
