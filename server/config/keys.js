759ecf709a05a93d5ab731748ad026576a050416
Mefik21 <seregakarpenko@gmail.com>
2022-01-17 18:56:26 +0300

    rwerwer

--- server/config/keys.js
+++ server/config/keys.js
@@ -8,4 +8,6 @@ module.exports = {
     googleClientSecret: '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',
     redirect_uris: ['http://localhost:5000/auth/google/callback'],
     javascript_origins: ['http://localhost:5000'],
+    mongoURI: 'mongodb+srv://mr_mefik_9999:U7kv8fUtDhyYFgf@emaily.sbfty.mongodb.net/emaily?retryWrites=true&w=majority',
+    cookieKey: 'asdad9&hiuoaf98y)*3124iouUoy34h_)_)(orwei!##3123'
 };
